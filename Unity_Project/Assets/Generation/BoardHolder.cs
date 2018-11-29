ac1be37acbca297223bd0aef5786a43e64a7f194
John Reimer <jreimer107@gmail.com>
2018-11-28 19:31:41 -0600

    Added entrance and exit tiles

--- Unity_Project/Assets/Generation/BoardHolder.cs
+++ Unity_Project/Assets/Generation/BoardHolder.cs
@@ -9,6 +9,8 @@ public class BoardHolder : MonoBehaviour {
 	public Tile wallTile;
 	public Tile borderTile;
 	public Tile pathTile;
+	public Tile entranceTile;
+	public Tile exitTile;
 	public GameObject player;
 
 	public Tilemap tilemap;
@@ -33,6 +35,12 @@ public class BoardHolder : MonoBehaviour {
 				}
 				else if (grid[x][y] == Floor.TileType.Border) {
 					tilemap.SetTile(position, borderTile);
+				}
+				else if (grid[x][y] == Floor.TileType.Entrance) {
+					tilemap.SetTile(position, entranceTile);
+				}
+				else if (grid[x][y] == Floor.TileType.Exit) {
+					tilemap.SetTile(position, exitTile);
 				}
 				else {
 					tilemap.SetTile(position, wallTile);
