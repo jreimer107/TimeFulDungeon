a6d3a6cf43249fa27613ae63800a1b95d40e278f
John Reimer <jreimer107@gmail.com>
2019-10-09 14:00:12 -0500

    Removed unused art, finished draft of weapon.cs

--- Unity_Project/Assets/Scripts/Weapon.cs
+++ Unity_Project/Assets/Scripts/Weapon.cs
@@ -3,45 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon{
-    public enum WeaponType {
+public class Weapon {
+	public enum WeaponType {
 		Melee, Ranged, Shield,
 	}
 	public WeaponType type;
 	public Sprite sprite;
+	public string name;
 
-	public Weapon(Sprite sprite) {
+	public Weapon(string name, Sprite sprite, WeaponType type) {
 		this.sprite = sprite;
+		this.name = name;
 	}
 }
 
 public class Melee : Weapon {
-	public int arcDegrees;
+	public float arc;
+	public int damage;
 	public int range;
 	public int speed;
+	public int cooldown;
 
-	public Melee(Sprite sprite, int arcDegrees, int range, int speed) : base(sprite) {
-		this.sprite = sprite;
-		this.arcDegrees = arcDegrees;
+	public Melee(string name, Sprite sprite, int damage, float arc, int range, int speed, int cooldown) : base(name, sprite, WeaponType.Melee) {
+		this.damage = damage;
+		this.arc = arc;
 		this.range = range;
 		this.speed = speed;
-		type = WeaponType.Melee;
-
+		this.cooldown = cooldown;
 	}
 }
 
 public class Ranged : Weapon {
-	public Projectile projectile;
-	public int rate;
+	public Sprite projectile;
+	public int damage, rate, speed, penetrate;
 
-	public Ranged(Sprite sprite, Projectile projectile, int rate) : base(sprite)  {
-		this.sprite = sprite;
+	public Ranged(string name, Sprite sprite, Sprite projectile, int damage, int rate, int speed, int penetrate) : base(name, sprite, WeaponType.Ranged) {
 		this.projectile = projectile;
+		this.damage = damage;
 		this.rate = rate;
+		this.speed = speed;
+		this.penetrate = penetrate;
+	}
+
+	public Projectile fire() {
+		return new Projectile(this.damage, this.speed, this.projectile, this.penetrate);
 	}
 }
-
-
 
 public class Projectile : MonoBehaviour {
 	public int damage;
@@ -49,7 +56,7 @@ public class Projectile : MonoBehaviour {
 	public Sprite sprite;
 	public int penetrate;
 
-	void Start(int damage, int speed, Sprite sprite, int penetrate) {
+	public Projectile(int damage, int speed, Sprite sprite, int penetrate) {
 		this.damage = damage;
 		this.speed = speed;
 		this.sprite = sprite;
@@ -58,5 +65,15 @@ public class Projectile : MonoBehaviour {
 
 	void FixedUpdate() {
 		//move projectile and check for enemies in its hitbox
+	}
+}
+
+public class Shield : Weapon {
+	public int staminaUse;
+	public float arc;
+
+	public Shield(string name, Sprite sprite, int staminaUse, float arc) : base(name, sprite, WeaponType.Shield) {
+		this.staminaUse = staminaUse;
+		this.arc = arc;
 	}
 }
