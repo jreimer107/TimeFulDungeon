62ac2e32fd2613e877b9e1631ef80bfb8a103c93
John Reimer <jreimer107@gmail.com>
2021-02-27 23:17:51 -0600

    Wander module - smoothed spawn ajust
    Changed ajust to be linear. Added adjust limit, where the entity will
    not be able to go past.

--- Unity_Project/Assets/Scripts/AI/WanderModule.cs
+++ Unity_Project/Assets/Scripts/AI/WanderModule.cs
@@ -9,50 +9,76 @@ using NoiseTest;
 /// a tendency to go back to spawn if too far away.
 /// </summary>
 public class WanderModule : MonoBehaviour {
-    /// <summary>
-    /// Where the NPC should wander towards.
-    /// </summary>
-    public  Vector2 WanderDirection { get; private set; }
+	/// <summary>
+	/// Where the NPC should wander towards.
+	/// </summary>
+	public  Vector2 WanderDirection { get; private set; }
 
-    [Tooltip("Distance from spawn when the entity will start to be guided back towards it.")]
-    [SerializeField] [Min(0)] private float spawnAdjustStart = 2;
-    [Tooltip("How many frames in between wander turn updates. More frames makes smoother turns.")]
-    [SerializeField] [Min(1)] private int directionChangeInverval = 20; 
+	[Tooltip("Distance from spawn when the entity will start to be guided back towards it.")]
+	[SerializeField] [Min(0)] private float spawnAdjustStart = 2;
+	[Tooltip("Distance from spawn past which the entity will head straight back to its spawn.")]
+	[SerializeField] [Min(0)] private float spawnAdjustLimit = 8;
+	[Tooltip("How many frames in between wander turn updates. More frames makes smoother turns.")]
+	[SerializeField] [Min(1)] private int directionChangeInverval = 20; 
 
-    private int frameCount = 0;
-    private float turnAmount = 0;
-    
-    private OpenSimplexNoise noise;
-    private new Rigidbody2D rigidbody;
-    private Vector2 spawn;
+	private int frameCount = 0;
+	private float turnAmount = 0;
+	
+	private OpenSimplexNoise noise;
+	private new Rigidbody2D rigidbody;
+	private Vector2 spawn;
+	private float spawnAdjustSlope;
 
-    private void Start() {
-        noise = new OpenSimplexNoise();
-        rigidbody = GetComponent<Rigidbody2D>();
-        WanderDirection = new Vector2(Random.value, Random.value);
-        spawn = transform.position;
-    }
+	private void Start() {
+		noise = new OpenSimplexNoise();
+		rigidbody = GetComponent<Rigidbody2D>();
+		WanderDirection = new Vector2(Random.value, Random.value);
+		spawn = transform.position;
+		CalculateSpawnAdjustSlope();
+	}
 
-    private void Update() {
-        // Only change our wander turning every so often to smooth turns
-        frameCount++;
-        if (frameCount >= directionChangeInverval) {
-            frameCount = 0;
-            turnAmount = (float) noise.Evaluate(transform.position.x, transform.position.y) * Mathf.Deg2Rad;
-        }
+	private void OnValidate() {
+		CalculateSpawnAdjustSlope();
+	}
 
-        // Turn our desired direction
-        Vector2 newWanderDirection = WanderDirection.Rotate(turnAmount);
+	private void Update() {
+		// Only change our wander turning every so often to smooth turns
+		frameCount++;
+		if (frameCount >= directionChangeInverval) {
+			frameCount = 0;
+			turnAmount = (float) noise.Evaluate(transform.position.x, transform.position.y) * Mathf.Deg2Rad;
+		}
 
-        // Check if we are too far from spawn, and weight going back if we are
-        Vector2 spawnDirection = spawn - (Vector2) transform.position;
-        float spawnDistance = spawnDirection.magnitude;
-        if (spawnDistance > spawnAdjustStart) {
-            float weight = 10 / (spawnDistance + 9);
-            newWanderDirection = newWanderDirection * weight + spawnDirection.normalized * (1 - weight);
-        }
+		// Turn our desired direction
+		Vector2 newWanderDirection = WanderDirection.Rotate(turnAmount);
 
-        WanderDirection = newWanderDirection;
-        Debug.DrawLine(transform.position, (Vector2) transform.position + WanderDirection, Color.red);
-    }
+		// Check if we are too far from spawn, and weight going back if we are
+		Vector2 spawnDirection = spawn - (Vector2) transform.position;
+		float spawnDistance = spawnDirection.magnitude;
+		if (spawnDistance > spawnAdjustStart) {
+			float weight = (spawnDistance - spawnAdjustStart) * spawnAdjustSlope;
+			newWanderDirection = newWanderDirection * (1 - weight) + spawnDirection.normalized * weight;
+			newWanderDirection.Normalize();
+		}
+
+		WanderDirection = newWanderDirection;
+		Debug.DrawLine(transform.position, (Vector2) transform.position + WanderDirection, Color.red);
+	}
+
+	/// <summary>
+	/// Uses a linear slope. 1 at start, 0.5 at limit.
+	/// 0.5 is used so that no further progress can be made, but the entity can sit still.
+	/// </summary>
+	private void CalculateSpawnAdjustSlope() {
+		if (spawnAdjustLimit < spawnAdjustStart) {
+			spawnAdjustLimit = spawnAdjustStart;
+			Debug.LogWarning("Wander module: spawn adjust limit less than adjust start.");
+		}
+		if (spawnAdjustStart == spawnAdjustLimit) {
+			spawnAdjustSlope = 0;
+		}
+		else {
+			spawnAdjustSlope = 0.5f / (spawnAdjustLimit - spawnAdjustStart);
+		}
+	}
 }
