51d0044d78cad2698e150165adfc67abc274f29f
John Reimer <jreimer107@gmail.com>
2021-03-04 18:25:10 -0600

    Refactored ChatBubble.cs

--- Unity_Project/Assets/Scripts/UI/ChatBubble.cs
+++ Unity_Project/Assets/Scripts/UI/ChatBubble.cs
@@ -1,54 +1,57 @@
-﻿using UnityEngine;
-using TMPro;
+﻿using TMPro;
+using UnityEngine;
 
 /// <summary>
-/// Chat bubble class. Allows creating a chat bubble that will follow gameobjects.
+///     Chat bubble class. Allows creating a chat bubble that will follow gameobjects.
 /// </summary>
 public class ChatBubble : MonoBehaviour {
+    #region Static fields and methods
 
-	private SpriteRenderer backgroundSprite;
-	private TextMeshPro textMeshPro;
-	private AutoType autoType;
+    private static ChatBubble chatBubblePrefab;
 
-	/// <summary>
-	/// Creates a chat bubble.
-	/// </summary>
-	/// <param name="parent">The gameobject to follow.</param>
-	/// <param name="localPosition">Offset from the gameobject's postion.</param>
-	/// <param name="text">The text to print.</param>
-	public static void Create(Transform parent, Vector3 localPosition, string text) {
-		Transform chatBubbleTrfm = Instantiate(Resources.Load<Transform>("Prefabs/ChatBubble"), parent);
-		chatBubbleTrfm.localPosition = localPosition;
-		chatBubbleTrfm.GetComponent<ChatBubble>().SetUp(text);
-	}
+    /// <summary>
+    ///     Creates a chat bubble.
+    /// </summary>
+    /// <param name="parent">The gameobject to follow.</param>
+    /// <param name="localPosition">Offset from the gameobject's postion.</param>
+    /// <param name="text">The text to print.</param>
+    public static void Create(Transform parent, Vector3 localPosition, string text) {
+        chatBubblePrefab ??= Resources.Load<ChatBubble>("Prefabs/ChatBubble");
 
-	private void Awake() {
-		backgroundSprite = transform.Find("Background").GetComponent<SpriteRenderer>();
-		textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
-		autoType = GetComponent<AutoType>();
-	}
+        var chatBubble = Instantiate(chatBubblePrefab, parent);
+        chatBubble.transform.localPosition = localPosition;
+        chatBubble.text = text;
+    }
 
-	private void Start() {
-		// Make background transparent
-		backgroundSprite.color = new Color(1, 1, 1, 0);
+    #endregion
 
-		// Remove text from chat bubble so that it can be autotyped in
-		string text = textMeshPro.text;
-		textMeshPro.text = "";
+    #region Instance fields and methods
 
-		// Tween in the alpha and then print the message
-		LeanTween.alpha(backgroundSprite.gameObject, 1f, 0.5f).setOnComplete(
-			() => autoType.PrintMessage(text, () => Destroy(gameObject, 3f))
-		);
-	}
+    private string text;
 
-	private void SetUp(string text) {
-		// Get the background the right size
-		textMeshPro.SetText(text);
-		textMeshPro.ForceMeshUpdate();
-		Vector2 textSize = textMeshPro.GetRenderedValues(false);
-		backgroundSprite.size = textSize + new Vector2(0.2f, 0.1f);
-		backgroundSprite.transform.localPosition = new Vector3(backgroundSprite.size.x / 2f - 0.1f, 0f);
-	}
+    private void Start() {
+        var backgroundSprite = transform.Find("Background").GetComponent<SpriteRenderer>();
+        var textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
+        var autoType = GetComponent<AutoType>();
 
+        // Get the background the right size
+        textMeshPro.text = text;
+        textMeshPro.ForceMeshUpdate();
+        var textSize = textMeshPro.GetRenderedValues(false);
+        backgroundSprite.size = textSize + new Vector2(0.2f, 0.1f);
+        backgroundSprite.transform.localPosition = new Vector3(backgroundSprite.size.x / 2f - 0.1f, 0f);
+
+        // Make background transparent
+        backgroundSprite.color = new Color(1, 1, 1, 0);
+
+        // Remove text from chat bubble so that it can be autotyped in
+        textMeshPro.text = "";
+
+        // Tween in the alpha and then print the message
+        LeanTween.alpha(backgroundSprite.gameObject, 1f, 0.5f).setOnComplete(
+            () => autoType.PrintMessage(text, () => Destroy(gameObject, 3f))
+        );
+    }
+
+    #endregion
 }
